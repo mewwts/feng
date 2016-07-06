957af83002c36ddaafe172f496b073542cd24167
Mats Julian Olsen <(none)>
2016-07-06 18:05:35 +0200

    Added package info

--- feng/__init__.py
+++ feng/__init__.py
@@ -1,3 +1,11 @@
 from . import importance
 from . import pipeline
 from . import preprocessing
+
+
+
+__title__ = 'feng'
+__version__ = '0.0.1'
+__author__ = 'Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
---
+++ setup.py
@@ -0,0 +1,33 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import feng
+
+SHORT='Feng is a module for feature engineering'
+LONG= SHORT + '. For more info check out the README at \'github.com/mewwts/feng\'.'
+
+setup(
+    name='feng',
+    version=feng.__version__,
+    packages=['feng'],
+    url='https://github.com/mewwts/feng',
+    author=feng.__author__,
+    author_email='mats@plysjbyen.net',
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 3'
+    ),
+    description=SHORT,
+    long_description=LONG,
+    test_suite='test_feng'
+)
