d69a2b854438dfd50ff7129ff2c061a974229884
Mats Julian Olsen <mats@plysjbyen.net>
2016-11-04 10:33:34 +0100

    0.2.0

--- feng/__init__.py
+++ feng/__init__.py
@@ -5,7 +5,7 @@ from . import preprocessing
 
 
 __title__ = 'feng'
-__version__ = '0.1.3'
+__version__ = '0.2.0'
 __author__ = 'Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
