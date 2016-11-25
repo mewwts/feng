1dd667e6e4a59d45898710a98ad068c4fd2e99a0
Mats Julian Olsen <mats@plysjbyen.net>
2016-11-25 12:09:43 +0100

    Fix install.

--- feng/__init__.py
+++ feng/__init__.py
@@ -1,11 +1,5 @@
-from . import importance
-from . import pipeline
-from . import preprocessing
-
-
-
 __title__ = 'feng'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
