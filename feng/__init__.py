62719f7a25d88969094c7ab6d43f3524a8abc8ca
Mats Julian Olsen <mats@plysjbyen.net>
2016-12-04 14:18:44 +0100

    proper importing of packages

--- feng/__init__.py
+++ feng/__init__.py
@@ -1,5 +1,9 @@
+import pipeline
+import importance
+import preprocessing
+
 __title__ = 'feng'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __author__ = 'Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
