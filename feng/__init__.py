c661c351666e8631ccb6660f6fd275349dd238f5
Mats Julian Olsen <mats@plysjbyen.net>
2017-02-26 10:00:25 +0100

    Version bump

--- feng/__init__.py
+++ feng/__init__.py
@@ -3,7 +3,7 @@ from . import importance
 from . import preprocessing
 
 __title__ = 'feng'
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 __author__ = 'Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Alexander Svanevik, Anders Aagard, Mats Julian Olsen'
